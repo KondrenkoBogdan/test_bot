106a0d3d12ba61c9ae0862e6682f8b73253dcbf1
bogdan <kondrenkobogdan2808@gmail.com>
2021-02-12 12:19:28 +0200

    6

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ connection.commit()
 
 WEBHOOK_HOST = '104.248.133.84'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '10.114.0.2'  # In some VPS you may need to put here the IP addr
+WEBHOOK_LISTEN = '104.248.133.84'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
